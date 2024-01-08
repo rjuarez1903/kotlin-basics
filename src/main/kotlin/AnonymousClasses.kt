9046a51107862c10e0598c2ad22af920b8254964
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2024-01-08 12:31:59 -0300

    add anonymous classes

---
+++ src/main/kotlin/AnonymousClasses.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val a = 5.0
+    val b = 2.0
+    val height = 2.0
+    // Anonymous class
+    // An anonymous class is a class that doesn't have a name
+    // We can use anonymous classes to create objects that extend a class or implement an interface
+    val parallelogram = object : Shape("Parallelogram", a, b, height) {
+        init {
+            println("$name created with base $a, side $b, and height $height")
+            println("$name area: ${area()}")
+            println("$name perimeter: ${perimeter()}")
+        }
+        override fun area() = a * height
+        override fun perimeter() = 2 * (a + b)
+
+        fun isRectangle(): Boolean = height == b
+    }
+    println("Parallelogram is a rectangle: ${parallelogram.isRectangle()}")
+}
--- src/main/kotlin/Shape.kt
+++ src/main/kotlin/Shape.kt
@@ -1,5 +1,9 @@
 // An abstract class is a class that cannot be instantiated, but can contain abstract methods
 abstract class Shape(var name: String) {
+
+    // This allows us to construct our custom shapes on the fly
+    constructor(name: String, vararg dimensions: Double): this(name)
+
     init {
         println("i am a super class")
     }
